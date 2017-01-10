764c61ff6c10dc553e2e2e015d39ef2943021c93
Ophelia Yin <owyowy1@gmail.com>
2017-01-10 07:57:32 -0800

    add handlebar, have not test it yet

--- projectView.js
+++ projectView.js
@@ -5,11 +5,15 @@ function Article (options) {
   this.title = options.title;
   this.articleUrl = options.articleUrl;
   this.publishedOn = options.publishedOn;
-  this.synopsis = this.synopsis;
+  this.synopsis = options.synopsis;
 };
-
+//handlebar template
+Article.prototype.toHtml = function() {
+  var template =
+  Handlebars.compile($('#projectView-template').text());
+  return template(this);
+}
 //render projects.js objects to html
-
 articleView.handleMainNav = function () {
 
   $('.main-nav').on('click', '.navigation', function() {

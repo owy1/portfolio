ee5eeeb1069f88c671afa79c48dba33cf9d7a6e6
Ophelia Yin <owyowy1@gmail.com>
2017-01-30 02:44:30 -0800

    proxy server

--- public/models/repos.js
+++ public/models/repos.js
@@ -5,18 +5,23 @@
 
   repos.all = [];
 
-  repos.requestRepos = function(callback) {
+  // repos.requestRepos = function(callback) {
+  //   $.ajax({
+  //     // url: 'https://api.github.com/users/owy1/repos',
+  //     url: 'https://api.github.com/user/repos?type=owner',
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: `token ${githubToken}`
+  //     }
+  //   }).then(data => {repos.all = data;
+  //     callback();})
+  // };
 
-    $.ajax({
-      // url: 'https://api.github.com/users/owy1/repos',
-      url: 'https://api.github.com/user/repos?type=owner',
-      method: 'GET',
-      headers: {
-        Authorization: `token ${githubToken}`
-      }
-    }).then(data => {repos.all = data;
-      callback();})
-  };
+  repos.requestRepos = function(callback) {
+      $.ajax('/github/user/repos')
+      .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+      .then(callback);
+    };
 
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
